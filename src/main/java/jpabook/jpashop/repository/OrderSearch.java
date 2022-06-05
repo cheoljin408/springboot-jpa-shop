21a07881980125a50f1d5d2f4e5a395b29073673
cheoljin408 <kcj409@naver.com>
2022-06-05 17:59:07 +0900

    주문 도메인 개발
    - 주문 검색 기능 개발

---
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+}
